9f8892cfc8b22de5a3c9da321830a56987c1c01e
XiNGRZ <chenxingyu92@gmail.com>
2021-10-11 13:39:47 +0530

    fleur: Add dtbo image to the output zip if present
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>
    Signed-off-by: DrtSinX98 <pritish@stag-os.org>
    Signed-off-by: Ritukanta Reddy <reddyritukanta1925@gmail.com>

--- releasetools.py
+++ releasetools.py
@@ -1,25 +1,28 @@
+#!/usr/bin/env python3
+
 import common
 import re
 
 
-def fullOTA_installed(info):
-    OTA_installed(info)
+def FullOTA_InstallEnd(info):
+    OTA_InstallEnd(info)
     return
 
 
-def incrementalOTA_installed(info):
-    OTA_installed(info)
+def IncrementalOTA_InstallEnd(info):
+    OTA_InstallEnd(info)
     return
 
 
-def add_images(info, basename, dest):
+def AddImage(info, basename, dest):
     name = basename
     data = info.input_zip.read("IMAGES/" + basename)
     common.ZipWriteStr(info.output_zip, name, data)
     info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
 
 
-def OTA_installed(info):
+def OTA_InstallEnd(info):
     info.script.Print("Patching firmware images...")
-    add_images(info, "vbmeta.img", "/dev/block/by-name/vbmeta")
+    AddImage(info, "dtbo.img", "/dev/block/by-name/dtbo")
+    AddImage(info, "vbmeta.img", "/dev/block/by-name/vbmeta")
     return
