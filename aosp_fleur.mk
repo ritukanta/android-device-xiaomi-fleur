e3183d9e8a1495b9f9c121ac3f47db96dff610ba
Ritukanta Reddy <reddyritukanta1925@gmail.com>
2022-10-30 03:17:20 +0000

    fleur: Update Build info
    From raven-user 13 TP1A.221005.002 9012097 release-keys (android-13.0.0_r7 2022-10-05)
    
    Signed-off-by: Ritukanta Reddy <reddyritukanta1925@gmail.com>

--- aosp_fleur.mk
+++ aosp_fleur.mk
@@ -27,12 +27,13 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 
 # Build info
-BUILD_FINGERPRINT := "google/raven/raven:13/TP1A.220905.004/8927612:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:13/TP1A.221005.002/9012097:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=fleur \
     PRODUCT_NAME=fleur \
-    PRIVATE_BUILD_DESC="raven-user 13 TP1A.220905.004 8927612 release-keys"
-
-    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+    PRIVATE_BUILD_DESC="raven-user 13 TP1A.221005.002 9012097 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
